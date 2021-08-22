4fc45593bdc33371ed89bfd2981ca057a2bfec02
Martin <mselvenis@gmail.com>
2021-08-22 22:05:49 +0100

    bulma stylesheets

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Hello World</h1>
+      <p class="subtitle">My first website with <strong>Bulma</strong>!</p>
+    </div>
+  </section>
 </template>
