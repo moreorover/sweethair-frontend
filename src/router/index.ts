e6294672830d29cbad6e7ff24048adb78937e97b
Martin <mselvenis@gmail.com>
2021-08-22 22:38:02 +0100

    Login component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import Login from "@/views/Login.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,11 +12,12 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: About,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
 ];
 
---
+++ src/views/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container has-text-centered">
+    <h1 class="title is-4">Log IN</h1>
+    <form @submit.prevent="onSubmit">
+      <div class="field">
+        <div class="control">
+          <input
+            v-model="email"
+            type="email"
+            class="input is-medium"
+            placeholder="email"
+          />
+        </div>
+      </div>
+      <div class="field">
+        <div class="control">
+          <input
+            v-model="password"
+            type="password"
+            class="input is-medium"
+            placeholder="password"
+          />
+        </div>
+      </div>
+      <button class="button is-block is-primary is-fullwidth is-medium">
+        Submit
+      </button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "Login",
+  setup() {
+    const email = ref("");
+    const password = ref("");
+
+    const onSubmit = () => {
+      // TODO login and redirect
+      console.log("submit");
+    };
+
+    return { email, password, onSubmit };
+  },
+});
+</script>
+
+<style></style>
