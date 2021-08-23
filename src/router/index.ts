b07579278173a0b6177f3836d368017e7a946f01
Martin <mselvenis@gmail.com>
2021-08-23 22:56:55 +0100

    router auth guards

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,8 +15,8 @@ const routes: Array<RouteRecordRaw> = [
     name: "Wrapper",
     component: Wrapper,
     children: [
-      { path: "", name: "Home", component: Home },
-      { path: "/about", name: "About", component: About },
+      { path: "", name: "Home", component: Home, meta: { requiresAuth: true } },
+      { path: "/about", name: "About", component: About, meta: { requiresAuth: true } },
     ],
   },
 ];
@@ -24,6 +24,25 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    // if (!auth.loggedIn()) {
+    //   next({
+    //     path: '/login',
+    //     query: { redirect: to.fullPath }
+    //   })
+    // } else {
+    //   next()
+    // }
+    console.log("route requires auth");
+    next();
+  } else {
+    next(); // make sure to always call next()!
+  }
 });
 
 export default router;
