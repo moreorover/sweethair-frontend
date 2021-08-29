711f2559d8af5fc916b602f0beff48d36bae4e80
Martin <mselvenis@gmail.com>
2021-08-29 19:15:17 +0100

    axios config

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
-export default axios.create({
+const apiClient = axios.create({
   baseURL: 'http://127.0.0.1:3000',
+  withCredentials: true,
+  // headers: {
+  //   Accept: 'application/json',
+  //   'Content-Type': 'application/json',
+  //   'Access-Control-Allow-Credentials': 'true',
+  // },
 });
+
+export default apiClient;
