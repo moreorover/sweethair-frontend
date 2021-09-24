0f162da9806cedb1d174e048da802cea896c8255
Martin <mselvenis@gmail.com>
2021-09-24 22:09:45 +0100

    fix: appointment customers array not always defined

--- src/components/appointments/AppointmentsTable.vue
+++ src/components/appointments/AppointmentsTable.vue
@@ -17,7 +17,7 @@
       >
         <tr @click="navigate">
           <td>{{ appointment.start }}</td>
-          <td>{{ appointment.customers.length }}</td>
+          <td>{{ appointment.customers?.length }}</td>
           <td class="level-right">
             <router-link :to="`/appointments/${appointment.id}/edit`" class="button is-small is-warning">
               Edit
