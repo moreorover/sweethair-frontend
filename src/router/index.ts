f787d3f1795a7b5c3ac83dece2f957e8390aa247
Martin <mselvenis@gmail.com>
2021-09-24 19:28:54 +0100

    fix: router using pinia user store

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,75 +11,80 @@ import Appointments from '@/views/appointments/Appointments.vue';
 import NewAppointment from '@/views/appointments/NewAppointment.vue';
 import Appointment from '@/views/appointments/Appointment.vue';
 import EditAppointment from '@/views/appointments/EditAppointment.vue';
-import { Store } from '@/store';
+import { useLoggedInUserStore } from '@/store/pinia/loggedInUser';
 
-export function routerWithStore(store: Store) {
-  const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes: [
-      {
-        path: '/login',
-        name: 'Login',
-        component: Login,
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes: [
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/logout',
+      name: 'Logout',
+      component: Login,
+      beforeEnter: () => {
+        // store.getAuth().logout();
+        return true;
       },
-      {
-        path: '/logout',
-        name: 'Logout',
-        component: Login,
-        beforeEnter: () => {
-          store.getAuth().logout();
-          return true;
+    },
+    {
+      path: '',
+      name: 'Wrapper',
+      component: Wrapper,
+      meta: { requiresAuth: true },
+      children: [
+        { path: '', name: 'Home', component: Home, meta: { requiresAuth: true } },
+        { path: '/about', name: 'About', component: About, meta: { requiresAuth: true } },
+        { path: '/customers/:id', name: 'Customer', component: Customer, meta: { requiresAuth: true } },
+        { path: '/customers/:id/edit', name: 'Edit Customer', component: EditCustomer, meta: { requiresAuth: true } },
+        { path: '/customers/new', name: 'New Customer', component: NewCustomer, meta: { requiresAuth: true } },
+        { path: '/customers', name: 'Customers', component: Customers, meta: { requiresAuth: true } },
+
+        { path: '/appointments/:id', name: 'Appointment', component: Appointment, meta: { requiresAuth: true } },
+        {
+          path: '/appointments/:id/edit',
+          name: 'Edit Appointment',
+          component: EditAppointment,
+          meta: { requiresAuth: true },
         },
-      },
-      {
-        path: '',
-        name: 'Wrapper',
-        component: Wrapper,
-        meta: { requiresAuth: true },
-        children: [
-          { path: '', name: 'Home', component: Home, meta: { requiresAuth: true } },
-          { path: '/about', name: 'About', component: About, meta: { requiresAuth: true } },
-          { path: '/customers/:id', name: 'Customer', component: Customer, meta: { requiresAuth: true } },
-          { path: '/customers/:id/edit', name: 'Edit Customer', component: EditCustomer, meta: { requiresAuth: true } },
-          { path: '/customers/new', name: 'New Customer', component: NewCustomer, meta: { requiresAuth: true } },
-          { path: '/customers', name: 'Customers', component: Customers, meta: { requiresAuth: true } },
+        {
+          path: '/appointments/new',
+          name: 'New Appointment',
+          component: NewAppointment,
+          meta: { requiresAuth: true },
+        },
+        { path: '/appointments', name: 'Appointments', component: Appointments, meta: { requiresAuth: true } },
+      ],
+    },
+  ],
+});
 
-          { path: '/appointments/:id', name: 'Appointment', component: Appointment, meta: { requiresAuth: true } },
-          {
-            path: '/appointments/:id/edit',
-            name: 'Edit Appointment',
-            component: EditAppointment,
-            meta: { requiresAuth: true },
-          },
-          {
-            path: '/appointments/new',
-            name: 'New Appointment',
-            component: NewAppointment,
-            meta: { requiresAuth: true },
-          },
-          { path: '/appointments', name: 'Appointments', component: Appointments, meta: { requiresAuth: true } },
-        ],
-      },
-    ],
-  });
+router.beforeEach((to, from, next) => {
+  const store = useLoggedInUserStore();
 
-  router.beforeEach(async (to, from, next) => {
-    await store.getAuth().me();
+  if (!to.meta.requiresAuth) {
+    next();
+    return;
+  }
 
-    const user = store.getAuth().getState().user;
+  if (to.meta.requiresAuth && store.isLoggedIn) {
+    return next();
+  } else {
+    return next({
+      path: '/login',
+    });
+  }
 
-    if (!to.meta.requiresAuth) {
-      next();
-      return;
-    }
+  // if (to.meta.requiresAuth && user) {
+  //   next();
+  // } else {
+  //   next({
+  //     path: '/login',
+  //   });
+  // }
+});
 
-    if (to.meta.requiresAuth && user) {
-      next();
-    } else {
-      next({
-        path: '/login',
-      });
-    }
-  });
-  return router;
-}
+export default router;
