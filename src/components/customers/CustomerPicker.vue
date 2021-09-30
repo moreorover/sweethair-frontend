2d29eb38bbd6739608f217b0a16a630c7198eccd
Martin <mselvenis@gmail.com>
2021-09-30 18:26:53 +0100

    Single customer picker

---
+++ src/components/customers/CustomerPicker.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="box">
+    <h1 class="title">Pick Customer</h1>
+    <nav class="level">
+      <div class="level-left">
+        <div class="level-item">
+          <p class="subtitle is-5">
+            <strong>{{ customers.length }}</strong> customers
+          </p>
+        </div>
+
+        <div class="level-item is-hidden-tablet-only">
+          <div class="field">
+            <p class="control">
+              <input v-model="searchKey" class="input" type="text" placeholder="Customer name, email..." />
+            </p>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <table v-if="filteredCustomers.length > 0" class="table is-fullwidth is-hoverable">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Full Name</th>
+          <th>Email</th>
+          <th>Instagram</th>
+        </tr>
+      </thead>
+      <tfoot>
+        <tr>
+          <th></th>
+          <th>Full Name</th>
+          <th>Email</th>
+          <th>Instagram</th>
+        </tr>
+      </tfoot>
+      <tbody>
+        <tr v-for="customer in filteredCustomers" :key="customer.id">
+          <td>
+            <input type="checkbox" :checked="selection?.id === customer.id" @click="toggleCustomer(customer)" />
+          </td>
+          <td>{{ customer.firstName }} {{ customer.lastName }}</td>
+          <td>{{ customer.email }}</td>
+          <td>{{ customer.instagram }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <p v-else-if="searchKey === ''" class="title has-text-info has-text-centered">
+      Search Customers by typing in search field.
+    </p>
+    <p v-else class="title has-text-danger has-text-centered">No Customers found, try something else.</p>
+  </div>
+</template>
+<script lang="ts">
+import { computed, defineComponent, ref } from 'vue';
+import { Customer } from '@/services/CustomerService';
+import { useCustomersStore } from '@/store/customersStore';
+
+export default defineComponent({
+  name: 'CustomerPicker',
+  // components: { BaseCheckbox },
+  props: {
+    customerSelection: {
+      type: Object as () => Customer,
+      required: true,
+    },
+  },
+  emits: ['selected'],
+  setup(props, { emit }) {
+    const customerStore = useCustomersStore();
+    const customers = computed(() => customerStore.getAll);
+    const searchKey = ref('');
+    const selection = ref<Customer | null>(props.customerSelection);
+
+    customerStore.fetchAll();
+
+    const filteredCustomers = computed(() => {
+      return customers.value.filter((customer) => {
+        if (searchKey.value === '' && !(props.customerSelection?.id === customer.id)) {
+          return false;
+        }
+        if (
+          customer.firstName.toLowerCase().includes(searchKey.value) ||
+          customer.lastName.toLowerCase().includes(searchKey.value) ||
+          customer.email.toLowerCase().includes(searchKey.value) ||
+          customer.instagram.toLowerCase().includes(searchKey.value) ||
+          props.customerSelection?.id === customer.id
+        ) {
+          return true;
+        }
+        return false;
+      });
+    });
+
+    const toggleCustomer = (customer: Customer) => {
+      if (selection.value?.id === customer.id) {
+        selection.value = null;
+      } else {
+        selection.value = customer;
+      }
+      emit('selected', selection.value);
+    };
+
+    return { filteredCustomers, searchKey, toggleCustomer, customers, selection };
+  },
+});
+</script>
+<style scoped></style>
