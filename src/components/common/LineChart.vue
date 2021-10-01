64f83cb45ff8657f9eaea6f664e5e2f0953d6ccf
Martin <mselvenis@gmail.com>
2021-10-01 21:01:39 +0100

    LineChart component

---
+++ src/components/common/LineChart.vue
@@ -0,0 +1,15 @@
+<template>
+  <line-chart :data="data" />
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    data: {
+      type: Array,
+      required: true,
+    },
+  },
+});
+</script>
