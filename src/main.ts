46c2956c8442afd6ffcddef7057e1f5e9a367245
Martin <mselvenis@gmail.com>
2021-10-01 21:01:13 +0100

    VueChartkick dependecy importing

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
+import VueChartkick from 'vue-chartkick';
+import 'chartkick/chart.js';
 
 const app = createApp(App);
+app.use(VueChartkick);
 app.use(createPinia());
 app.use(router);
 
