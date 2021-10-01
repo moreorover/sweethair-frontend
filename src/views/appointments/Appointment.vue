c7ed09d9340a2ea935f35652287b6aa972f77e0f
Martin <mselvenis@gmail.com>
2021-10-01 22:43:23 +0100

    template updated

--- src/views/appointments/Appointment.vue
+++ src/views/appointments/Appointment.vue
@@ -1,34 +1,43 @@
 <template>
-  <h1 class="title">Appointment</h1>
-  <h1 class="subtitle">{{ appointment?.start && format(new Date(appointment?.start), 'dd MMMM yyyy HH:mm') }}</h1>
-  <section class="section">
-    <customers-cards :customers="appointment?.customers" />
-  </section>
+  <h1 class="title block">Appointment</h1>
+
+  <h1 class="subtitle block">{{ appointment?.start && format(new Date(appointment?.start), 'dd MMMM yyyy HH:mm') }}</h1>
+  <router-link :to="`/appointments/${appointment?.id}/edit`" class="button is-small is-warning block">
+    Edit
+  </router-link>
+  <div class="columns is-desktop">
+    <div v-for="customer in customers" :key="customer.id" class="column">
+      <customer-card :customer="customer" />
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent, computed } from 'vue';
 import { useRoute } from 'vue-router';
 import { useAppointmentsStore } from '@/store/appointmentsStore';
 import { format } from 'date-fns';
-import CustomersCards from '@/components/customers/CustomersCards.vue';
+import CustomerCard from '@/components/customers/CustomerCard.vue';
+import { useCustomersStore } from '@/store/customersStore';
 
 export default defineComponent({
-  components: { CustomersCards },
+  components: { CustomerCard },
   setup() {
-    const store = useAppointmentsStore();
+    const appointmentsStore = useAppointmentsStore();
+    const customersStore = useCustomersStore();
     const id = useRoute().params.id as string;
 
-    store.fetchAll();
+    appointmentsStore.fetchAll();
+    customersStore.fetchAll();
 
-    const appointment = computed(() => store.getCustomerById(id));
-
+    const appointment = computed(() => appointmentsStore.getCustomerById(id));
+    const customers = computed(() => customersStore.getCustomersByAppointment(appointment.value));
     if (!appointment.value) {
       throw Error('Appointment was not found.');
     }
 
-    store.setSelected(appointment.value);
+    appointmentsStore.setSelected(appointment.value);
 
-    return { appointment, format };
+    return { appointment, format, customers };
   },
 });
 </script>
