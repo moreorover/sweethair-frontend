cbbbcc0048be520b3a8c3214d39b149394b1e770
Martin <mselvenis@gmail.com>
2021-10-02 21:35:05 +0100

    prop two way binding to parent

--- src/components/customers/CustomerForm.vue
+++ src/components/customers/CustomerForm.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="column">
-    <form @submit.prevent="onSubmit">
+    <form @submit.prevent="">
       <div class="field">
         <div class="field">
           <label class="label">First Name</label>
           <div class="control">
-            <input v-model="firstName" class="input is-large" type="text" placeholder="e.g. Jane" required />
+            <input v-model="customer.firstName" class="input is-large" type="text" placeholder="e.g. Jane" required />
           </div>
         </div>
       </div>
@@ -14,7 +14,7 @@
         <div class="field">
           <label class="label">Last Name</label>
           <div class="control">
-            <input v-model="lastName" class="input is-large" type="text" placeholder="e.g. Smith" required />
+            <input v-model="customer.lastName" class="input is-large" type="text" placeholder="e.g. Smith" required />
           </div>
         </div>
       </div>
@@ -22,7 +22,7 @@
       <div class="field">
         <label class="label">Email</label>
         <div class="control has-icons-left">
-          <input v-model="email" class="input" type="email" placeholder="e.g. janesmith@gmail.com" />
+          <input v-model="customer.email" class="input" type="email" placeholder="e.g. janesmith@gmail.com" />
           <span class="icon is-small is-left">
             <i class="fa fa-envelope"></i>
           </span>
@@ -32,55 +32,53 @@
       <div class="field">
         <label class="label">Instagram</label>
         <div class="control">
-          <input v-model="instagram" class="input" type="text" placeholder="janesmith" />
+          <input v-model="customer.instagram" class="input" type="text" placeholder="janesmith" />
         </div>
       </div>
 
-      <div class="field">
+      <!-- <div class="field">
         <div class="buttons">
           <button class="button is-medium is-success">Save</button>
         </div>
-      </div>
+      </div> -->
     </form>
   </div>
 </template>
 <script lang="ts">
 import { Customer } from '@/services/CustomerService';
-import { defineComponent, ref } from 'vue';
+import { computed, defineComponent } from 'vue';
 
 export default defineComponent({
   props: {
-    customer: {
+    customerValue: {
       type: Object as () => Customer,
       required: true,
     },
   },
-  emits: ['save'],
-  setup(props, ctx) {
-    const firstName = ref(props.customer.firstName);
-    const lastName = ref(props.customer.lastName);
-    const email = ref(props.customer.email);
-    const instagram = ref(props.customer.instagram);
+  emits: ['update:customerValue'],
+  setup(props, { emit }) {
+    const customer = computed({
+      get: () => props.customerValue,
+      set: (val) => emit('update:customerValue', val),
+    });
 
-    const onSubmit = () => {
-      const newCustomer: Customer = {
-        ...props.customer,
-        firstName: firstName.value,
-        lastName: lastName.value,
-        email: email.value,
-        instagram: instagram.value,
-      };
+    return { customer };
 
-      for (var propName in newCustomer) {
-        if (newCustomer[propName] === null || newCustomer[propName] === undefined || newCustomer[propName] === '') {
-          delete newCustomer[propName];
-        }
-      }
+    // const onSubmit = () => {
+    //   const newCustomer: Customer = {
+    //     ...props.customer,
+    //     firstName: firstName.value,
+    //     lastName: lastName.value,
+    //     email: email.value,
+    //     instagram: instagram.value,
+    //   };
 
-      ctx.emit('save', newCustomer);
-    };
-
-    return { firstName, lastName, email, instagram, onSubmit };
+    //   for (var propName in newCustomer) {
+    //     if (newCustomer[propName] === null || newCustomer[propName] === undefined || newCustomer[propName] === '') {
+    //       delete newCustomer[propName];
+    //     }
+    //   }
+    // };
   },
 });
 </script>
