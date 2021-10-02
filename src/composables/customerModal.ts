decff627194308369c29c1150e16e686c5a2ef96
Martin <mselvenis@gmail.com>
2021-10-02 21:38:04 +0100

    customer composable

---
+++ src/composables/customerModal.ts
@@ -0,0 +1,24 @@
+import { Customer } from '@/services/CustomerService';
+
+const emptyCustomer: Customer = { firstName: '', lastName: '', email: '', instagram: '' };
+
+export const useCustomerModal = function () {
+  const cleanCustomer = (customer: Customer): Customer => {
+    for (const propName in customer) {
+      if (customer[propName] === null || customer[propName] === undefined || customer[propName] === '') {
+        delete customer[propName];
+      }
+      delete customer.createdOn;
+      delete customer.modifiedOn;
+    }
+    return customer;
+  };
+
+  return {
+    emptyCustomer,
+
+    cleanCustomer,
+  };
+};
+
+export default useCustomerModal;
