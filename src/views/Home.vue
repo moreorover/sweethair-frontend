f0400652a67015bbbcdd5b42c1d45558f7f2ad33
Martin <mselvenis@gmail.com>
2021-10-05 21:59:56 +0100

    Statistics elements

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,48 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <nav class="level box">
+    <div class="level-item has-text-centered">
+      <div>
+        <p class="heading">Customers</p>
+        <p class="title">{{ cCount }}</p>
+      </div>
+    </div>
+    <div class="level-item has-text-centered">
+      <div>
+        <p class="heading">Appointments</p>
+        <p class="title">{{ aCount }}</p>
+      </div>
+    </div>
+    <div class="level-item has-text-centered">
+      <div>
+        <p class="heading">Transactions</p>
+        <p class="title">{{ tCount }}</p>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { useAppointmentsStore } from '@/store/appointmentsStore';
+import { useCustomersStore } from '@/store/customersStore';
+import { useTransactionsStore } from '@/store/transactionsStore';
+import { computed, defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
+  setup() {
+    const customersStore = useCustomersStore();
+    const appointmentsStore = useAppointmentsStore();
+    const transactionsStore = useTransactionsStore();
+
+    customersStore.fetchAll();
+    appointmentsStore.fetchAll();
+    transactionsStore.fetchAll();
+
+    const cCount = computed(() => customersStore.getAll.length);
+    const aCount = computed(() => appointmentsStore.getAll.length);
+    const tCount = computed(() => transactionsStore.getAll.length);
+
+    return { cCount, aCount, tCount };
   },
 });
 </script>
