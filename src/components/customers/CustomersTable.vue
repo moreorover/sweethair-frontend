8c456d580c6eca6fa671ad9c707905a7f7580b6c
Martin <mselvenis@gmail.com>
2021-10-05 22:00:23 +0100

    book transaction from sustomer table

--- src/components/customers/CustomersTable.vue
+++ src/components/customers/CustomersTable.vue
@@ -27,6 +27,7 @@
           <div class="buttons">
             <customer-modal title="Edit Customer" action="Edit" :customer="customer" />
             <appointment-modal title="Book Appointment" action="Book Appointment" :customers="[customer]" />
+            <transaction-modal title="Book New Transaction" action="New Transaction" :customer="customer" />
           </div>
         </td>
       </tr>
@@ -39,10 +40,11 @@ import { Customer } from '@/services/CustomerService';
 import { useCustomersStore } from '@/store/customersStore';
 import CustomerModal from '@/components/customers/CustomerModal.vue';
 import AppointmentModal from '../appointments/AppointmentModal.vue';
+import TransactionModal from '@/components/transactions/TransactionModal.vue';
 
 export default defineComponent({
   name: 'CustomersTable',
-  components: { AppointmentModal, CustomerModal },
+  components: { AppointmentModal, CustomerModal, TransactionModal },
   props: {
     customers: {
       type: Object as () => Customer[],
