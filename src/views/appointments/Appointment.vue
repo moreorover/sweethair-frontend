196be52d5990a0ed6f96e3273e96a17b4ac7ea71
Martin <mselvenis@gmail.com>
2021-10-08 22:31:29 +0100

    appointment template updated

--- src/views/appointments/Appointment.vue
+++ src/views/appointments/Appointment.vue
@@ -1,13 +1,26 @@
 <template>
   <h1 class="title block">Appointment</h1>
 
-  <h1 class="subtitle block">{{ appointment?.start && format(new Date(appointment?.start), 'dd MMMM yyyy HH:mm') }}</h1>
-  <router-link :to="`/appointments/${appointment?.id}/edit`" class="button is-small is-warning block">
-    Edit
-  </router-link>
-  <div class="columns is-desktop block">
-    <div v-for="customer in customers" :key="customer.id" class="column">
-      <customer-card :customer="customer" />
+  <nav class="level">
+    <div class="level-left">
+      <div class="level-item">
+        <h1 class="subtitle block">
+          {{ appointment?.start && format(new Date(appointment?.start), 'dd MMMM yyyy HH:mm') }}
+        </h1>
+      </div>
+      <div class="level-item">
+        <appointment-modal title="Edit Appointment" action="Edit" :appointment="appointment" />
+      </div>
+    </div>
+  </nav>
+  <!-- <customers-table :customers="customers" /> -->
+  <div class="columns is-multiline content">
+    <div v-for="customer in customers" :key="customer.id" class="column is-full">
+      <div class="box">
+        <h1 class="title">{{ customer.firstName }} {{ customer.lastName }}</h1>
+
+        <div v-for="transaction in customer.transactions" :key="transaction.id"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,28 +29,33 @@ import { defineComponent, computed } from 'vue';
 import { useRoute } from 'vue-router';
 import { useAppointmentsStore } from '@/store/appointmentsStore';
 import { format } from 'date-fns';
-import CustomerCard from '@/components/customers/CustomerCard.vue';
 import { useCustomersStore } from '@/store/customersStore';
+import AppointmentModal from '@/components/appointments/AppointmentModal.vue';
+import { useTransactionsStore } from '@/store/transactionsStore';
 
 export default defineComponent({
-  components: { CustomerCard },
+  components: { AppointmentModal },
   setup() {
     const appointmentsStore = useAppointmentsStore();
     const customersStore = useCustomersStore();
+    const transactionsStore = useTransactionsStore();
     const id = useRoute().params.id as string;
 
     appointmentsStore.fetchAll();
     customersStore.fetchAll();
+    transactionsStore.fetchAll();
 
-    const appointment = computed(() => appointmentsStore.getCustomerById(id));
+    const appointment = computed(() => appointmentsStore.getAppointmentById(id));
     const customers = computed(() => customersStore.getCustomersByAppointment(appointment.value));
+    const transactions = computed(() => transactionsStore.getTransactionsByAppointment(appointment.value));
+
     if (!appointment.value) {
       throw Error('Appointment was not found.');
     }
 
     appointmentsStore.setSelected(appointment.value);
 
-    return { appointment, format, customers };
+    return { appointment, format, customers, transactions };
   },
 });
 </script>
