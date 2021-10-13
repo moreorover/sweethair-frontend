3060e56ac3dc908947301e77dc93f864e74f9b1d
Martin <mselvenis@gmail.com>
2021-10-13 21:38:11 +0100

    Customer CU

---
+++ src/components/customers/EditCustomer.vue
@@ -0,0 +1,38 @@
+<template>
+  <modal title="Edit Customer" action="Edit" @submit="submit">
+    <customer-form :customer-value="customer" />
+  </modal>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useCustomersStore } from '@/store/customersStore';
+import Modal from '@/components/common/Modal.vue';
+import CustomerForm from '@/components/customers/CustomerForm.vue';
+import useEntityCleaner from '@/composables/entityCleaner';
+import { Customer } from '@/services/CustomerService';
+
+export default defineComponent({
+  name: 'EditCustomerModal',
+  components: { Modal, CustomerForm },
+  props: {
+    customerValue: {
+      type: Object as () => Customer,
+      required: true,
+    },
+  },
+  setup(props) {
+    const store = useCustomersStore();
+    const entityCleaner = useEntityCleaner();
+
+    const customer: Customer = { ...props.customerValue };
+
+    const submit = () => {
+      const cleanCustomer: Customer = entityCleaner.clean(customer);
+      store.update(cleanCustomer);
+    };
+
+    return { customer, submit };
+  },
+});
+</script>
+<style scoped></style>
---
+++ src/components/customers/NewCustomer.vue
@@ -0,0 +1,43 @@
+<template>
+  <modal title="Create New" action="New" @submit="submit">
+    <customer-form :customer-value="newCustomer" />
+  </modal>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useCustomersStore } from '@/store/customersStore';
+import Modal from '@/components/common/Modal.vue';
+import CustomerForm from '@/components/customers/CustomerForm.vue';
+import useEntityCleaner from '@/composables/entityCleaner';
+import { Customer } from '@/services/CustomerService';
+
+export default defineComponent({
+  name: 'NewCustomerModal',
+  components: { Modal, CustomerForm },
+  setup() {
+    const store = useCustomersStore();
+    const entityCleaner = useEntityCleaner();
+
+    const newCustomer: Customer = {
+      id: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      instagram: '',
+    };
+
+    const submit = () => {
+      const cleanCustomer: Customer = entityCleaner.clean(newCustomer);
+      store.create(cleanCustomer);
+      newCustomer.id = '';
+      newCustomer.firstName = '';
+      newCustomer.lastName = '';
+      newCustomer.email = '';
+      newCustomer.instagram = '';
+    };
+
+    return { newCustomer, submit };
+  },
+});
+</script>
+<style scoped></style>
