1cb6478d0294de0620b1517c8923c9d3223efeb9
Martin <mselvenis@gmail.com>
2021-10-18 19:46:51 +0100

    customers table component

--- src/components/customers/CustomersTable.vue
+++ src/components/customers/CustomersTable.vue
@@ -1,62 +1,84 @@
 <template>
-  <table class="table is-fullwidth is-hoverable">
-    <thead>
-      <tr>
-        <th>Full Name</th>
-        <th>Email</th>
-        <th>Instagram</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-    <tfoot>
-      <tr>
-        <th>Full Name</th>
-        <th>Email</th>
-        <th>Instagram</th>
-        <th>Actions</th>
-      </tr>
-    </tfoot>
-    <tbody>
-      <tr v-for="customer in customers" :key="customer.id">
-        <router-link v-slot="{ navigate }" :to="`/customers/${customer.id}`" custom>
-          <td @click="navigate()">{{ customer.firstName }} {{ customer.lastName }}</td>
-          <td @click="navigate()">{{ customer.email }}</td>
-          <td @click="navigate()">{{ customer.instagram }}</td>
+  <DataTable
+    v-model:filters="filters"
+    :value="customers"
+    responsive-layout="stack"
+    class="p-datatable-sm"
+    :paginator="true"
+    :rows="10"
+    filter-display="menu"
+    :global-filter-fields="['firstName', 'lastName', 'email', 'instagram']"
+    paginator-template="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+    :rows-per-page-options="[10, 25, 50]"
+    current-page-report-template="Showing {first} to {last} of {totalRecords} entries"
+  >
+    <template #header>
+      <div class="p-d-flex p-jc-between p-ai-center">
+        <h5 class="p-m-0">Customers</h5>
+
+        <div class="flex">
+          <new-customer />
+          <span class="p-input-icon-left">
+            <i class="pi pi-search" />
+            <InputText v-model="filters['global'].value" placeholder="Keyword Search" />
+          </span>
+        </div>
+      </div>
+    </template>
+    <template #empty> No customers found. </template>
+    <Column field="firstName" header="First Name" :sortable="true"></Column>
+    <Column field="lastName" header="Last Name" :sortable="true"></Column>
+    <Column field="email" header="Email"></Column>
+    <Column field="instagram" header="Instagram"></Column>
+    <Column header-style="width: 4rem; text-align: center" body-style="text-align: center; overflow: visible">
+      <template #body="slotProps">
+        <edit-customer :customer-value="slotProps.data" />
+      </template>
+    </Column>
+    <Column
+      v-if="viewCustomer"
+      header-style="width: 4rem; text-align: center"
+      body-style="text-align: center; overflow: visible"
+    >
+      <template #body="slotProps">
+        <router-link v-slot="{ navigate }" :to="{ name: `Customer`, params: { id: slotProps.data.id } }" custom>
+          <Button class="p-button-sm" type="button" icon="pi pi-search" @click="navigate" />
         </router-link>
-        <td>
-          <div class="buttons">
-            <customer-modal title="Edit Customer" action="Edit" :customer="customer" />
-            <appointment-modal title="Book Appointment" action="Book Appointment" :customers="[customer]" />
-            <transaction-modal title="Book New Transaction" action="New Transaction" :customer="customer" />
-          </div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+      </template>
+    </Column>
+  </DataTable>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { Customer } from '@/services/CustomerService';
-import { useCustomersStore } from '@/store/customersStore';
-import CustomerModal from '@/components/customers/CustomerModal.vue';
-import AppointmentModal from '../appointments/AppointmentModal.vue';
-import TransactionModal from '@/components/transactions/TransactionModal.vue';
+import NewCustomer from '@/components/customers/NewCustomer.vue';
+import EditCustomer from '@/components/customers/EditCustomer.vue';
+import { FilterMatchMode, FilterOperator } from 'primevue/api';
 
 export default defineComponent({
   name: 'CustomersTable',
-  components: { AppointmentModal, CustomerModal, TransactionModal },
+  components: { NewCustomer, EditCustomer },
   props: {
     customers: {
       type: Object as () => Customer[],
       required: true,
     },
+    viewCustomer: {
+      type: Boolean,
+      default: false,
+    },
   },
+  emits: ['rowClicked'],
   setup() {
-    const customersStore = useCustomersStore();
-    const selectCustomer = (customer: Customer) => {
-      customersStore.selectCustomer(customer);
-    };
-    return { selectCustomer };
+    const filters = ref({
+      global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+      name: {
+        operator: FilterOperator.AND,
+        constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
+      },
+    });
+
+    return { filters };
   },
 });
 </script>
--- src/views/customers/Customers.vue
+++ src/views/customers/Customers.vue
@@ -1,70 +1,23 @@
 <template>
   <div class="px-0 py-4 md:px-4">
     <div class="border-2 surface-border border-round surface-card">
-      <DataTable
-        v-model:filters="filters"
-        :value="customers"
-        responsive-layout="scroll"
-        class="p-datatable-sm"
-        :paginator="true"
-        :rows="10"
-        filter-display="menu"
-        :global-filter-fields="['firstName', 'lastName', 'email', 'instagram']"
-        paginator-template="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-        :rows-per-page-options="[10, 25, 50]"
-        current-page-report-template="Showing {first} to {last} of {totalRecords} entries"
-        @row-click="rowClicked"
-      >
-        <template #header>
-          <div class="p-d-flex p-jc-between p-ai-center">
-            <h5 class="p-m-0">Customers</h5>
-
-            <div class="flex">
-              <new-customer />
-              <span class="p-input-icon-left">
-                <i class="pi pi-search" />
-                <InputText v-model="filters['global'].value" placeholder="Keyword Search" />
-              </span>
-            </div>
-          </div>
-        </template>
-        <template #empty> No customers found. </template>
-        <Column field="firstName" header="First Name" :sortable="true"></Column>
-        <Column field="lastName" header="Last Name" :sortable="true"></Column>
-        <Column field="email" header="Email"></Column>
-        <Column field="instagram" header="Instagram"></Column>
-        <Column header-style="width: 4rem; text-align: center" body-style="text-align: center; overflow: visible">
-          <template #body="slotProps">
-            <edit-customer :customer-value="slotProps.data" />
-          </template>
-        </Column>
-      </DataTable>
+      <customers-table :customers="customers" :view-customer="true" @rowClicked="rowClicked" />
     </div>
   </div>
 </template>
 <script lang="ts">
-import { computed, defineComponent, ref } from 'vue';
+import { computed, defineComponent } from 'vue';
 import { useCustomersStore } from '@/store/customersStore';
-import NewCustomer from '@/components/customers/NewCustomer.vue';
-import EditCustomer from '@/components/customers/EditCustomer.vue';
-import { FilterMatchMode, FilterOperator } from 'primevue/api';
 import router from '@/router';
+import CustomersTable from '@/components/customers/CustomersTable.vue';
 
 export default defineComponent({
   name: 'Customers',
-  components: { NewCustomer, EditCustomer },
+  components: { CustomersTable },
   setup() {
     const store = useCustomersStore();
 
     store.fetchAll();
-
-    const filters = ref({
-      global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-      name: {
-        operator: FilterOperator.AND,
-        constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
-      },
-    });
 
     const customers = computed(() => store.getAll);
 
@@ -72,7 +25,7 @@ export default defineComponent({
       router.push({ name: 'Customer', params: { id: data.data.id } });
     };
 
-    return { customers, filters, rowClicked };
+    return { customers, rowClicked };
   },
 });
 </script>
