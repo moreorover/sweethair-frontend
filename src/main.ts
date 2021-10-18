58d9d74c08dd51b3d1b9c45dd3f50c8eb16695c5
Martin <mselvenis@gmail.com>
2021-10-18 21:03:15 +0100

    importing componenets

--- src/main.ts
+++ src/main.ts
@@ -15,11 +15,14 @@ import StyleClass from 'primevue/styleclass';
 
 import Badge from 'primevue/badge';
 import Button from 'primevue/button';
+import Calendar from 'primevue/calendar';
 import Card from 'primevue/card';
 import Column from 'primevue/column';
 import ColumnGroup from 'primevue/columngroup';
 import DataTable from 'primevue/datatable';
 import Dialog from 'primevue/dialog';
+import InputNumber from 'primevue/inputnumber';
+import InputSwitch from 'primevue/inputswitch';
 import InputText from 'primevue/inputtext';
 
 const app = createApp(App);
@@ -32,11 +35,14 @@ app.directive('styleclass', StyleClass);
 
 app.component('Badge', Badge);
 app.component('Button', Button);
+app.component('Calendar', Calendar);
 app.component('Card', Card);
 app.component('Column', Column);
 app.component('ColumnGroup', ColumnGroup);
 app.component('DataTable', DataTable);
 app.component('Dialog', Dialog);
+app.component('InputNumber', InputNumber);
+app.component('InputSwitch', InputSwitch);
 app.component('InputText', InputText);
 
 app.mount('#app');
