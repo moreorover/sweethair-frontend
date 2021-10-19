479c3fed4c3db8b808f7cc894b23596ba7dd0ba8
Martin <mselvenis@gmail.com>
2021-10-19 14:10:15 +0100

    customer picker

--- src/components/transactions/TransactionForm.vue
+++ src/components/transactions/TransactionForm.vue
@@ -24,14 +24,28 @@
         />
       </div>
     </div>
+    <div class="p-field p-grid">
+      <label for="isPaid" class="p-col-12 p-mb-2 p-md-2 p-mb-md-0">Customer</label>
+      <div v-if="transaction.customer" class="p-col-2">
+        {{ transaction.customer?.firstName }} {{ transaction.customer?.lastName }}
+      </div>
+      <div class="p-col-10 p-md-8">
+        <modal title="Select Customer" action="Select Customer">
+          <single-customer-picker v-model:selection-value="transaction.customer" />
+        </modal>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import { Transaction } from '@/services/TransactionService';
 import { defineComponent, computed } from 'vue';
+import SingleCustomerPicker from '../customers/SingleCustomerPicker.vue';
+import Modal from '@/components/common/Modal.vue';
 
 export default defineComponent({
   name: 'TransactionForm',
+  components: { SingleCustomerPicker, Modal },
   props: {
     transactionValue: {
       type: Object as () => Transaction,
