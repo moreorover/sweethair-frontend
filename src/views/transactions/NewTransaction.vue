907c59e5418acab182090a6121f7c048a545fe5c
Martin <mselvenis@gmail.com>
2021-10-19 14:10:03 +0100

    additional props

--- src/views/transactions/NewTransaction.vue
+++ src/views/transactions/NewTransaction.vue
@@ -1,6 +1,7 @@
 <template>
-  <modal title="Create New" action="New" @submit="submit">
+  <modal title="Create New" :action="action" @submit="submit">
     <transaction-form :transaction-value="newTransaction" />
+    {{ newTransaction }}
   </modal>
 </template>
 <script lang="ts">
@@ -10,13 +11,32 @@ import { useTransactionsStore } from '@/store/transactionsStore';
 import { Transaction } from '@/services/TransactionService';
 import Modal from '@/components/common/Modal.vue';
 import useEntityCleaner from '@/composables/entityCleaner';
+import { Customer } from '@/services/CustomerService';
+import { Appointment } from '@/services/AppointmentService';
 
 export default defineComponent({
   components: {
     TransactionForm,
     Modal,
   },
-  setup() {
+  props: {
+    action: {
+      type: String,
+      required: false,
+      default: 'New',
+    },
+    customer: {
+      type: Object as () => Customer,
+      required: false,
+      default: null,
+    },
+    appoointment: {
+      type: Object as () => Appointment,
+      required: false,
+      default: null,
+    },
+  },
+  setup(props) {
     const transactionsStore = useTransactionsStore();
     const entityCleaner = useEntityCleaner();
 
@@ -25,8 +45,8 @@ export default defineComponent({
       total: 0,
       date: new Date().toISOString(),
       isPaid: false,
-      customer: null,
-      appointment: null,
+      customer: props.customer,
+      appointment: props.appoointment,
     });
 
     const submit = () => {
