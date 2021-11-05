8b24eb43012222b328b900941c3efddf8be9b361
Martin <mselvenis@gmail.com>
2021-11-05 22:40:25 +0000

    fix: prop atribute

--- src/hooks/modelWrapper.ts
+++ src/hooks/modelWrapper.ts
@@ -2,7 +2,7 @@ import { computed } from 'vue';
 
 export function useModelWrapper(props, emit, name = 'modelValue') {
   return computed({
-    get: () => props.modelValue,
+    get: () => props[name],
     set: (value) => emit(`update:${name}`, value),
   });
 }
