42e363a2a1f351b0cbaa1031775d43347566b7e3
Martin <mselvenis@gmail.com>
2021-11-08 20:21:57 +0000

    BaseButton component

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button
+    :label="props.label"
+    @click="emit('onClick')"
+    class="
+      px-4
+      py-2
+      font-medium
+      tracking-wide
+      text-white
+      capitalize
+      transition-colors
+      duration-200
+      transform
+      bg-indigo-600
+      rounded-md
+      hover:bg-indigo-500
+      focus:outline-none focus:bg-indigo-500
+    "
+    v-bind="attrs"
+  >
+    {{ props.label }}
+  </button>
+</template>
+
+<script setup lang="ts">
+import { useAttrs } from 'vue';
+
+const attrs = useAttrs();
+type Props = {
+  label?: string;
+};
+
+const props = withDefaults(defineProps<Props>(), { label: '' });
+const emit = defineEmits(['onClick']);
+</script>
