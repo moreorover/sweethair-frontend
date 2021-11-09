b5606b5f477be2319d09ea8a58dfbd7901236d91
Martin <mselvenis@gmail.com>
2021-11-09 14:04:30 +0000

    new customer button size

--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -4,7 +4,7 @@
   <div class="mt-8">
     <div class="mt-6">
       <div class="flex flex-col mt-3 sm:flex-row">
-        <customer-dialog header="Create new Customer" label="New" />
+        <customer-dialog header="Create new Customer" label="New" buttonSize="medium" />
         <div class="flex">
           <div class="relative">
             <select
