cf5c74c890e432b00a07759e824eaf7936ffd2d5
Martin <mselvenis@gmail.com>
2021-11-09 13:50:22 +0000

    templating

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -3,9 +3,6 @@
     :label="props.label"
     @click="emit('onClick')"
     class="
-      px-4
-      py-2
-      font-medium
       tracking-wide
       text-white
       capitalize
@@ -17,6 +14,7 @@
       hover:bg-indigo-500
       focus:outline-none focus:bg-indigo-500
     "
+    :class="sizes.get(props.size as Size)"
     v-bind="attrs"
   >
     {{ props.label }}
@@ -26,11 +24,20 @@
 <script setup lang="ts">
 import { useAttrs } from 'vue';
 
+type Size = 'small' | 'medium' | 'large';
+
+const sizes = new Map<Size, string>([
+  ['small', 'px-2 py-0 font-normal'],
+  ['medium', 'px-4 py-2 font-medium'],
+  ['large', 'px-6 py-4 font-bold'],
+]);
+
 const attrs = useAttrs();
 type Props = {
   label?: string;
+  size: string;
 };
 
-const props = withDefaults(defineProps<Props>(), { label: '' });
+const props = withDefaults(defineProps<Props>(), { label: '', size: 'small' });
 const emit = defineEmits(['onClick']);
 </script>
