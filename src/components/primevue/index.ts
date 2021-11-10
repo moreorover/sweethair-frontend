33f3755a2c5a7774dcdf21e2c4d10a8aef25253e
Martin <mselvenis@gmail.com>
2021-11-10 17:43:28 +0000

    Calendar component registered

--- src/components/primevue/index.ts
+++ src/components/primevue/index.ts
@@ -3,12 +3,14 @@ import PrimeVue from 'primevue/config';
 import StyleClass from 'primevue/styleclass';
 
 import Button from 'primevue/button';
+import Calendar from 'primevue/calendar';
 import Dialog from 'primevue/dialog';
 
 const register = (app: App<Element>): void => {
   app.use(PrimeVue, { ripple: true });
   app.directive('styleclass', StyleClass);
   app.component('Button', Button);
+  app.component('Calendar', Calendar);
   app.component('Dialog', Dialog);
 };
 
