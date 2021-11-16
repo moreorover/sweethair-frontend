264ac3d88314cd74e13d8cbc25ba169aceab80cc
Martin <mselvenis@gmail.com>
2021-11-16 22:20:01 +0000

    pick customers

--- src/views/appointments/Appointment.vue
+++ src/views/appointments/Appointment.vue
@@ -2,6 +2,20 @@
   <div class="flex justify-between">
     <h3 class="text-3xl font-medium text-gray-700">{{ appointment?.title }}</h3>
     <AppointmentDialog :appointment="appointment" header="Edit Appointment" label="Edit" buttonSize="large" />
+    <MultipleCustomerPickerDialog
+      :preSelection="appointment?.customers"
+      header="Pick customers"
+      label="Pick Customers"
+      buttonSize="medium"
+      @submit="pickedCustomers($event)"
+    />
+  </div>
+  <div class="flex justify-center">
+    <div class="rounded-lg w-full">
+      <ul class="divide-y divide-gray-300">
+        <li v-for="customer in appointment?.customers">{{ customer.fullName }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
@@ -9,6 +23,8 @@ import { computed } from 'vue';
 import { useAppointmentsStore } from '@/store/appointmentsStore';
 import { Appointment } from '@/services/AppointmentService';
 import AppointmentDialog from '@/components/appointments/AppointmentDialog.vue';
+import MultipleCustomerPickerDialog from '@/components/customers/MultipleCustomerPickerDialog.vue';
+import { Customer } from '@/services/CustomerService';
 
 type Props = {
   id: string;
@@ -21,4 +37,11 @@ const appointmentsStore = useAppointmentsStore();
 const all = appointmentsStore.all;
 
 const appointment = computed<Appointment | undefined>(() => all.find((a) => a.id === props.id));
+
+const pickedCustomers = (customers: Customer[]) => {
+  if (appointment.value) {
+    appointment.value?.customers?.push(...customers);
+    appointmentsStore.update(appointment.value);
+  }
+};
 </script>
