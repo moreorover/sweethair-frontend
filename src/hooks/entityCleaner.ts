08030a8758224af33cf98d0e82095a26af00fa1f
Martin <mselvenis@gmail.com>
2021-11-19 14:21:34 +0000

    conditional to delete empty strings

--- src/hooks/entityCleaner.ts
+++ src/hooks/entityCleaner.ts
@@ -1,7 +1,10 @@
 export const useEntityCleaner = () => {
-  const clean = (entity) => {
+  const clean = (entity: any, emptyString: boolean = false) => {
     for (const propName in entity) {
       if (entity[propName] == null || entity[propName] === undefined) {
+        delete entity[propName];
+      }
+      if (emptyString && entity[propName] === '') {
         delete entity[propName];
       }
     }
