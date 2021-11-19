0251efd1a11e256a58adc9e6629029e10f52517a
Martin <mselvenis@gmail.com>
2021-11-19 15:16:10 +0000

    store rewrite into map

--- src/store/appointmentsStore.ts
+++ src/store/appointmentsStore.ts
@@ -1,90 +1,103 @@
 import AppointmentService, { Appointment } from '@/services/AppointmentService';
 import { defineStore } from 'pinia';
-import _ from 'lodash';
-import router from '@/router';
 import { Customer } from '@/services/CustomerService';
 import { getMonth, getYear } from 'date-fns';
 
 interface AppointmentStore {
-  all: Appointment[];
+  all: Record<string, Appointment>;
+  ids: string[];
   loaded: boolean;
+  loading: boolean;
 }
 
 export const useAppointmentsStore = defineStore({
   id: 'appointmentsStore',
-  state: (): AppointmentStore => ({ all: [] as Appointment[], loaded: false }),
+  state: (): AppointmentStore => ({
+    all: {},
+    ids: [],
+    loaded: false,
+    loading: false,
+  }),
   getters: {
-    getAll(state) {
+    getRaw(state): Record<string, Appointment> {
       return state.all;
     },
-    getIsLoaded(state) {
-      return state.loaded;
+    getAll(state): Appointment[] {
+      return state.ids.map((id) => this.all[id]);
     },
-    getAppointmentById: (state) => (id: string) => {
-      if (state.loaded) {
-        return state.all.find((x) => x.id === id);
-      } else {
-        console.log('State is not loaded.');
-      }
+    getIds(state): string[] {
+      return state.ids;
     },
-    getAppointmentByMonthAndYear: (state) => (month: number, year: number) => {
-      if (state.loaded) {
-        return state.all.filter(
-          (x) => getMonth(new Date(x.scheduledAt)) == month && getYear(new Date(x.scheduledAt)) == year
-        );
-      } else {
-        console.log('State is not loaded.');
-      }
-    },
+    getAppointmentById:
+      (state) =>
+      (id: string): Appointment => {
+        return state.all[id];
+      },
+    getAppointmentsByMonthAndYear:
+      (state) =>
+      (month: number, year: number): Appointment[] => {
+        return state.ids
+          .map((id) => state.all[id])
+          .filter((x) => getMonth(new Date(x.scheduledAt)) == month && getYear(new Date(x.scheduledAt)) == year);
+      },
     getAppointmentsByCustomer:
       (state) =>
-      (customer: Customer | undefined): Appointment[] => {
-        if (customer) {
-          const customerId: string | undefined = customer.id;
-          const appointments: Appointment[] = state.all.filter((a) =>
-            a.customers?.map((c) => c.id).includes(customerId)
-          );
-          return appointments;
-        }
-        return [];
+      (customer: Customer): Appointment[] => {
+        const { id: customerId } = customer;
+        const appointments: Appointment[] = state.ids
+          .map((id) => state.all[id])
+          .filter((a) => a.customers?.map((c) => c.id).includes(customerId));
+        return appointments;
       },
+    shouldLoadState: (state): boolean => !state.loading && !state.loaded,
   },
   actions: {
     async fetchAll() {
-      await AppointmentService.getAll()
-        .then((response) => {
-          this.all = response.data;
-          this.loaded = true;
-        })
-        .catch(() => {
-          this.loaded = false;
-          console.log('Not loaded, something went wrong loading Appointments');
-        });
+      this.loading = true;
+      try {
+        const { data } = await AppointmentService.getAll();
+        const ids: string[] = [];
+        const all: Record<string, Appointment> = {};
+
+        for (const appointment of data) {
+          if (!ids.includes(appointment.id)) {
+            ids.push(appointment.id);
+          }
+          all[appointment.id] = appointment;
+        }
+        this.all = all;
+        this.ids = ids;
+        this.loaded = true;
+        this.loading = false;
+      } catch (error) {
+        this.all = {};
+        this.ids = [];
+        this.loaded = false;
+        this.loading = false;
+        console.log('Not loaded, something went wrong loading Appointments');
+        console.log({ error });
+      }
     },
     async create(appointment: Appointment) {
-      await AppointmentService.create(appointment)
-        .then((response) => {
-          this.all.push(response.data);
-          this.all = _.sortBy(this.all, ['scheduledAt']);
-          router.push({ name: 'Appointment', params: { id: response.data.id } });
-        })
-        .catch((err) => console.log('Failed to create Customer', appointment, err));
+      try {
+        const { data } = await AppointmentService.create(appointment);
+        this.all[data.id] = data;
+        this.ids.push(data.id);
+      } catch (error) {
+        console.log('Failed to create Appointment', appointment, error);
+      }
     },
     async update(appointment: Appointment) {
-      await AppointmentService.update(appointment)
-        .then((response) => {
-          const i = this.all.findIndex((c) => c.id === response.data.id);
-          if (i > -1) {
-            this.all[i] = response.data;
-          } else {
-            this.all.push(response.data);
-          }
-        })
-        .catch((err) => console.log('Failed to update Appointment', appointment, err));
+      try {
+        const { data } = await AppointmentService.update(appointment);
+        this.all[data.id] = data;
+      } catch (error) {
+        console.log('Failed to update Appointment', appointment, error);
+      }
     },
-    async removeCustomer(appointment: Appointment | undefined, customer: Customer) {
-      if (appointment?.customers) {
-        appointment.customers = appointment.customers?.filter((c) => c.id !== customer.id);
+    async removeCustomer(appointment: Appointment, customer: Customer) {
+      if (appointment.customers) {
+        appointment.customers = appointment.customers.filter((c) => c.id !== customer.id);
         this.update(appointment);
       }
     },
