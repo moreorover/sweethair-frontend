b36683cfda1fa63717e4e853353ad0e142288835
Martin <mselvenis@gmail.com>
2021-11-22 10:22:38 +0000

    working Customers test

--- src/views/customers/Customers.spec.ts
+++ src/views/customers/Customers.spec.ts
@@ -1,9 +1,10 @@
 import { mount } from '@vue/test-utils';
 import ShowCustomers from '@/views/customers/ShowCustomers.vue';
-import flushPromises from 'flush-promises';
+import { flushPromises } from '@vue/test-utils';
 import MockAdapter from 'axios-mock-adapter';
-import axios from 'axios';
+import apiClient from '@/axios/axios';
 import { createTestingPinia } from '@pinia/testing';
+import router from '../../router';
 
 import BaseInput from '@/components/base/BaseInput.vue';
 import BaseButton from '@/components/base/BaseButton.vue';
@@ -16,27 +17,42 @@ import { useCustomersStore } from '@/store/customersStore';
 import { customers } from './mockData';
 
 describe('Customers', () => {
-  it('jada jada', async () => {
-    const mock = new MockAdapter(axios);
-    mock.onGet('/customers').reply(200, customers);
+  const mock = new MockAdapter(apiClient);
+  mock.onGet('/customers').reply(200, customers);
 
+  mock.onGet('/auth/me').reply(200, {
+    createdOn: '2021-08-26T20:16:44.175Z',
+    modifiedOn: '2021-08-26T20:16:53.312Z',
+    id: '5a1bfa0a-a988-4fc5-af72-442f7f549fed',
+    firstName: 'Admin',
+    lastName: 'Sweet',
+    email: 'test@gmail.com',
+    role: {
+      id: 1,
+      name: 'Admin',
+    },
+  });
+
+  it('jada jada', async () => {
     const wrapper = mount(ShowCustomers, {
       global: {
-        plugins: [createTestingPinia({ stubActions: false })],
+        plugins: [createTestingPinia({ stubActions: false }), router],
         components: { BaseInput, BaseButton, BaseCardGrid, BaseConfirm, BaseModal, Button, Dialog },
       },
     });
 
-    const customersStore = useCustomersStore();
     await flushPromises();
 
+    const customersStore = useCustomersStore();
+
     const title = wrapper.find('[data-test="title"]');
-    const cs = wrapper.find('[data-test="cs"]');
+    const cs = wrapper.find('[data-test="customer-card-c40e8d86-d675-4b7c-8a87-efce1858f12f"]');
 
     expect(title.text()).toEqual('Customers');
     expect(customersStore.fetchAll).toHaveBeenCalledTimes(1);
-    expect(mock.history.get.length).toBe(1);
-    expect(customersStore.all).toEqual(customers);
+    expect(mock.history.get.length).toBe(2);
+    expect(customersStore.getAll).toEqual(customers);
     expect(customersStore.fetchAll).toHaveBeenLastCalledWith();
+    expect(cs.text()).toContain('1113234234');
   });
 });
