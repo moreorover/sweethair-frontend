d1e115d56c8449f87aeb207e63403fd7a9f0891e
Martin <mselvenis@gmail.com>
2021-11-26 19:37:34 +0000

    week toolbar

---
+++ src/components/WeekToolBar.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="flex items-center justify-center mb-4">
+    <button
+      v-if="weekNumber != currentWeek"
+      @click="reset"
+      class="
+        text-purple-500
+        bg-transparent
+        border-l border-t border-b border-purple-500
+        hover:bg-purple-500 hover:text-white
+        active:bg-purple-600
+        font-bold
+        uppercase
+        text-xs
+        px-4
+        py-2
+        rounded-l
+        outline-none
+        focus:outline-none
+        mb-1
+        ease-linear
+        transition-all
+        duration-150
+      "
+      type="button"
+    >
+      Reset
+    </button>
+    <button
+      @click="decreaseWeek"
+      class="
+        text-purple-500
+        bg-transparent
+        border-l border-t border-b border-purple-500
+        hover:bg-purple-500 hover:text-white
+        active:bg-purple-600
+        font-bold
+        uppercase
+        text-xs
+        px-4
+        py-2
+        rounded-l
+        outline-none
+        focus:outline-none
+        mb-1
+        ease-linear
+        transition-all
+        duration-150
+      "
+      type="button"
+    >
+      - Week
+    </button>
+    <button
+      class="
+        border border-solid border-purple-500
+        text-white
+        bg-purple-500
+        font-bold
+        uppercase
+        text-xs
+        px-4
+        py-2
+        outline-none
+        focus:outline-none
+        mb-1
+        ease-linearp
+        transition-all
+        duration-150
+      "
+      type="button"
+    >
+      {{ weekStart.format('DD MMM yyyy') }} - {{ weekEnd.format('DD MMM yyyy') }}
+    </button>
+
+    <button
+      @click="increaseWeek"
+      class="
+        text-purple-500
+        bg-transparent
+        border-t border-b border-r border-purple-500
+        hover:bg-purple-500 hover:text-white
+        active:bg-purple-600
+        font-bold
+        uppercase
+        text-xs
+        px-4
+        py-2
+        rounded-r
+        outline-none
+        focus:outline-none
+        mb-1
+        ease-linear
+        transition-all
+        duration-150
+      "
+      type="button"
+    >
+      + Week
+    </button>
+  </div>
+</template>
+<script setup lang="ts">
+import { useWeek } from '@/hooks/useWeek';
+
+const { currentWeek, weekNumber, weekStart, weekEnd, increaseWeek, decreaseWeek, reset } = useWeek();
+</script>
---
+++ src/hooks/useWeek.ts
@@ -0,0 +1,33 @@
+import { computed, ref } from 'vue';
+import moment from 'moment';
+
+const currentDate = moment();
+const currentWeek = currentDate.week();
+const weekNumber = ref(currentWeek);
+
+export const useWeek = () => {
+  const weekStart = computed(() => moment().week(weekNumber.value).startOf('week'));
+  const weekEnd = computed(() => moment().week(weekNumber.value).endOf('week'));
+
+  const increaseWeek = () => {
+    ++weekNumber.value;
+  };
+
+  const decreaseWeek = () => {
+    --weekNumber.value;
+  };
+
+  const reset = () => {
+    weekNumber.value = currentWeek;
+  };
+
+  return {
+    currentWeek,
+    weekNumber,
+    weekStart,
+    weekEnd,
+    increaseWeek,
+    decreaseWeek,
+    reset,
+  };
+};
