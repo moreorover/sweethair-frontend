1a184add79c3c2e059dff3aca927c5f495728ac6
Martin <mselvenis@gmail.com>
2021-12-03 21:49:16 +0000

    items store

---
+++ src/store/itemsStore.ts
@@ -0,0 +1,81 @@
+import ItemService, { Item } from '@/services/ItemService';
+import { defineStore } from 'pinia';
+
+interface ItemStore {
+  all: Record<string, Item>;
+  ids: number[];
+  loaded: boolean;
+  loading: boolean;
+}
+
+export const useItemsStore = defineStore({
+  id: 'itemsStore',
+  state: (): ItemStore => ({
+    all: {},
+    ids: [],
+    loaded: false,
+    loading: false,
+  }),
+  getters: {
+    getRaw(state): Record<string, Item> {
+      return state.all;
+    },
+    getAll(state): Item[] {
+      return state.ids.map((id: number) => this.all[id]);
+    },
+    getIds(state): number[] {
+      return state.ids;
+    },
+    getItemById:
+      (state) =>
+      (id: number): Item => {
+        return state.all[id];
+      },
+    shouldLoadState: (state): boolean => !state.loading && !state.loaded,
+  },
+  actions: {
+    async fetchAll() {
+      this.loading = true;
+      try {
+        const { data } = await ItemService.getAll();
+        const ids: number[] = [];
+        const all: Record<string, Item> = {};
+
+        for (const item of data) {
+          if (!ids.includes(item.id)) {
+            ids.push(item.id);
+          }
+          all[item.id] = item;
+        }
+        this.all = all;
+        this.ids = ids;
+        this.loaded = true;
+        this.loading = false;
+      } catch (error) {
+        this.all = {};
+        this.ids = [];
+        this.loaded = false;
+        this.loading = false;
+        console.log('Not loaded, something went wrong loading Items');
+        console.log({ error });
+      }
+    },
+    async create(item: Item) {
+      try {
+        const { data } = await ItemService.create(item);
+        this.all[data.id] = data;
+        this.ids.push(data.id);
+      } catch (error) {
+        console.log('Failed to create Item', item, error);
+      }
+    },
+    async update(item: Item) {
+      try {
+        const { data } = await ItemService.update(item);
+        this.all[data.id] = data;
+      } catch (error) {
+        console.log('Failed to update Item', item, error);
+      }
+    },
+  },
+});
