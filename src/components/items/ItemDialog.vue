4d02e8e21fcb965b519e2160d8dcf816cb687487
Martin <mselvenis@gmail.com>
2021-12-04 22:44:42 +0000

    item dialog

---
+++ src/components/items/ItemDialog.vue
@@ -0,0 +1,56 @@
+<template>
+  <BaseButton :label="props.label" @onClick="toggleModal()" v-bind="attrs" />
+  <BaseModal :header="props.header" :show-footer="false" v-model:visible="showModal" @toggle-modal="toggleModal()">
+    <ItemForm :item="props.item" @submit="submit" />
+  </BaseModal>
+</template>
+<script setup lang="ts">
+import useEntityCleaner from '@/hooks/entityCleaner';
+import BaseModal from '@/components/base/BaseModal.vue';
+import useModal from '@/hooks/useModal';
+import BaseButton from '@/components/base/BaseButton.vue';
+import { Customer } from '@/services/CustomerService';
+import { Appointment } from '@/services/AppointmentService';
+import { useAttrs } from 'vue';
+import { Item } from '@/services/ItemService';
+import { useItemsStore } from '@/store/itemsStore';
+import ItemForm from '@/components/items/ItemForm.vue';
+
+interface Props {
+  item?: Item;
+  header: string;
+  label: string;
+  customer?: Customer;
+  appointment?: Appointment;
+}
+
+const attrs = useAttrs();
+const props = withDefaults(defineProps<Props>(), {
+  item: () => {
+    return {
+      id: -1,
+      total: 0.0,
+      title: '',
+    };
+  },
+  customer: undefined,
+  appointment: undefined,
+});
+
+const itemsStore = useItemsStore();
+const entityCleaner = useEntityCleaner();
+const { showModal, toggleModal } = useModal();
+
+const submit = async (item: Item) => {
+  const cleanItem: Item = entityCleaner.clean(
+    {
+      ...item,
+      customer: props.customer || null,
+      appointment: props.appointment || null,
+    },
+    true
+  );
+  cleanItem.id ? await itemsStore.update(cleanItem) : await itemsStore.create(cleanItem);
+  toggleModal();
+};
+</script>
