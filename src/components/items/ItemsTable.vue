5e823f5b9761a80a46ab7c50d845863b8897faa0
Martin <mselvenis@gmail.com>
2021-12-07 21:25:18 +0000

    id column

--- src/components/items/ItemsTable.vue
+++ src/components/items/ItemsTable.vue
@@ -7,6 +7,9 @@
             <thead class="bg-indigo-100">
               <tr>
                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Id
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Title
                 </th>
                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -19,6 +22,15 @@
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
               <tr v-for="item in props.items" :key="item.id">
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="flex items-center">
+                    <div class="ml-4">
+                      <div class="text-sm font-medium text-gray-900">
+                        {{ item.id }}
+                      </div>
+                    </div>
+                  </div>
+                </td>
                 <td class="px-6 py-4 whitespace-nowrap">
                   <div class="flex items-center">
                     <div class="ml-4">
