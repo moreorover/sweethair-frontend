250029c6172b8c050aca243b605d39f0e8b14ed7
Martin <mselvenis@gmail.com>
2021-12-07 00:39:06 +0000

    invoice

---
+++ src/views/invoices/ShowInvoice.vue
@@ -0,0 +1,14 @@
+<template>
+  <Suspense>
+    <template #default>
+      <Invoice />
+    </template>
+
+    <template #fallback>
+      <div class="flex mx-auto p-10">Loading...</div>
+    </template>
+  </Suspense>
+</template>
+<script setup lang="ts">
+import Invoice from '@/views/invoices/Invoice.vue';
+</script>
