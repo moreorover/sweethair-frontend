598a35289a8e0d26ac47b55ef9b528ec2e4011d5
Martin <mselvenis@gmail.com>
2021-12-10 16:07:58 +0000

    base confirm dialog

---
+++ src/components/base/BaseConfirmDialog.vue
@@ -0,0 +1,30 @@
+<template>
+  <BaseButton :label="props.label" @onClick="toggleModal()" v-bind="attrs" />
+  <BaseModal
+    :header="props.header"
+    :show-footer="true"
+    v-model:visible="showModal"
+    @submit="emit('confirm')"
+    @toggle-modal="toggleModal()"
+    @cancel="toggleModal()"
+  >
+    <slot></slot>
+  </BaseModal>
+</template>
+
+<script setup lang="ts">
+import { useAttrs } from 'vue';
+import useModal from '@/hooks/useModal';
+
+const attrs = useAttrs();
+const emit = defineEmits(['confirm']);
+
+type Props = {
+  header: string;
+  label: string;
+};
+
+const props = defineProps<Props>();
+
+const { showModal, toggleModal } = useModal();
+</script>
