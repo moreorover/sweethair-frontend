ec0c4bfde35116b451aba6fdef8bef2b236a810b
Martin <mselvenis@gmail.com>
2021-12-11 17:34:48 +0000

    appointment service

--- src/services/AppointmentService.ts
+++ src/services/AppointmentService.ts
@@ -2,6 +2,7 @@ import { Item } from './ItemService';
 import { Transaction } from './TransactionService';
 import { Customer } from '@/services/CustomerService';
 import Service, { DataEntity } from './Service';
+import { AxiosResponse } from 'axios';
 
 export interface Appointment extends DataEntity {
   scheduledAt: string;
@@ -11,6 +12,18 @@ export interface Appointment extends DataEntity {
   items?: Item[];
 }
 
-class AppointmentService extends Service<Appointment> {}
+class AppointmentService extends Service<Appointment> {
+  fetchCustomers(appointmentId: number): Promise<AxiosResponse<Customer[]>> {
+    return this.apiClient.get<Customer[]>(`${this.apiEndpoint}/${appointmentId}/customers`);
+  }
+
+  fetchTransactions(appointmentId: number): Promise<AxiosResponse<Transaction[]>> {
+    return this.apiClient.get<Transaction[]>(`${this.apiEndpoint}/${appointmentId}/transactions`);
+  }
+
+  fetchItems(appointmentId: number): Promise<AxiosResponse<Item[]>> {
+    return this.apiClient.get<Item[]>(`${this.apiEndpoint}/${appointmentId}/items`);
+  }
+}
 
 export default new AppointmentService('appointments');
