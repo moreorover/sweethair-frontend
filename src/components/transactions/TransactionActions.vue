158a11d1227f1d4e5a373acf1d81bb87f9ebba0f
Martin <mselvenis@gmail.com>
2021-12-30 22:19:35 +0000

    removed pick customer

--- src/components/transactions/TransactionActions.vue
+++ src/components/transactions/TransactionActions.vue
@@ -21,15 +21,6 @@
     @confirm="changePaidStatus"
     class="text-link px-1"
   />
-
-  <SingleCustomerPickerDialog
-    v-if="!props.transaction.customerId && !props.transaction.invoiceId"
-    header="Pick Customer"
-    label="Pick Customer"
-    :customers="props.appointment?.customers || []"
-    class="text-link px-1"
-    @submit="customerSelected($event)"
-  />
 </template>
 
 <script setup lang="ts">
@@ -38,7 +29,6 @@ import { Customer } from '@/services/CustomerService';
 import { Transaction } from '@/services/TransactionService';
 import TransactionDialog from '@/components/transactions/TransactionDialog.vue';
 import { useTransactionsStore } from '@/store/transactionsStore';
-import SingleCustomerPickerDialog from '../customers/SingleCustomerPickerDialog.vue';
 
 type Props = {
   transaction: Transaction;
@@ -59,9 +49,5 @@ const changePaidStatus = async () => {
 
 const deleteTransaction = async () => {
   await transactionsStore.delete(props.transaction);
-};
-
-const customerSelected = async (customer: Customer) => {
-  await transactionsStore.update({ ...props.transaction, customer });
 };
 </script>
