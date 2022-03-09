584cd576f77f47f0cc403e334ad366f7b119b47c
Martin <mselvenis@gmail.com>
2022-03-09 21:59:31 +0000

    supplier page

---
+++ src/views/suppliers/ShowSupplier.vue
@@ -0,0 +1,14 @@
+<template>
+  <Suspense>
+    <template #default>
+      <Supplier />
+    </template>
+
+    <template #fallback>
+      <div class="flex mx-auto p-10">Loading...</div>
+    </template>
+  </Suspense>
+</template>
+<script setup lang="ts">
+import Supplier from './Supplier.vue';
+</script>
---
+++ src/views/suppliers/Supplier.vue
@@ -0,0 +1,18 @@
+<template>
+  <h3 class="text-3xl font-medium text-gray-700">{{ supplier.fullName }}</h3>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useSuppliersStore } from '@/store/suppliersStore';
+import SuppliersTable from '@/components/suppliers/SuppliersTable.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const store = useSuppliersStore();
+const id: number = +route.params.id;
+
+if (!store.loaded) await store.fetchAll();
+
+const supplier = computed(() => store.getRaw[id]);
+</script>
