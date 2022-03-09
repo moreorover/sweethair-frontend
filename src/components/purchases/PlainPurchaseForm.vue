b638c93284a6d0428d515415f736db04f7c81e86
Martin <mselvenis@gmail.com>
2022-03-09 22:01:25 +0000

    plain purchase form

---
+++ src/components/purchases/PlainPurchaseForm.vue
@@ -0,0 +1,62 @@
+<template>
+  <FormKit type="form" @submit="register" v-model="purchase">
+    <h1>Purchase Form</h1>
+    <FormKit
+      type="date"
+      name="orderedAt"
+      label="Ordered at"
+      help="Enter a date when purchase was made"
+      validation="required"
+      validation-visibility="live"
+    />
+    <FormKit
+      type="date"
+      name="arrivesAt"
+      label="Arrives at"
+      help="Enter a date when purchase should arrive"
+      validation="required"
+      validation-visibility="live"
+    />
+    <FormKit
+      type="checkbox"
+      name="arrived"
+      label="Has the shipment already arrived?"
+      help="Check if shipment delivered."
+      validation-visibility="dirty"
+    />
+  </FormKit>
+</template>
+
+<script setup lang="ts">
+import { Purchase } from '@/types';
+import moment from 'moment';
+import { reactive } from 'vue';
+import { formatISO } from 'date-fns';
+
+type Props = {
+  purchase: Purchase;
+};
+
+const props = defineProps<Props>();
+
+const emit = defineEmits<{
+  (e: 'submit', purchase: Purchase): void;
+}>();
+
+const purchase = reactive<Purchase>({
+  ...props.purchase,
+  orderedAt: moment(props.purchase.orderedAt).format('YYYY-MM-DD'),
+  arrivesAt: moment(props.purchase.arrivesAt).format('YYYY-MM-DD'),
+});
+
+const register = () => {
+  emit('submit', {
+    arrived: purchase.arrived,
+    id: purchase.id,
+    total: purchase.total,
+    supplierId: purchase.supplierId,
+    orderedAt: formatISO(new Date(purchase.orderedAt)),
+    arrivesAt: formatISO(new Date(purchase.arrivesAt)),
+  });
+};
+</script>
