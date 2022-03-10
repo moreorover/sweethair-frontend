ec2bc7f2b2d387f2eb49d2d827a27ce405811d1a
Martin <mselvenis@gmail.com>
2022-03-10 19:48:53 +0000

    template updated

--- src/views/suppliers/Suppliers.vue
+++ src/views/suppliers/Suppliers.vue
@@ -1,16 +1,82 @@
 <template>
-  <h3 class="text-3xl font-medium text-gray-700">Suppliers</h3>
+  <h3 class="text-3xl font-medium text-gray-700 pb-4">Suppliers</h3>
 
-  <FormKit type="button" @click="newSupplier"> New Supplier </FormKit>
+  <Toolbar class="mb-4">
+    <template #start>
+      <SupplierFormDialog />
+    </template>
+  </Toolbar>
 
-  <SuppliersTable :suppliers="suppliers" />
+  <DataTable
+    ref="dt"
+    :value="suppliers"
+    dataKey="id"
+    :paginator="true"
+    :rows="10"
+    :filters="filters"
+    paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+    :rowsPerPageOptions="[5, 10, 25]"
+    currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
+    responsiveLayout="scroll"
+  >
+    <template #header>
+      <div
+        class="
+          table-header
+          flex flex-column
+          md:flex-row md:justiify-content-between
+        "
+      >
+        <h5 class="mb-2 md:m-0 p-as-md-center">Manage Suppliers</h5>
+        <span class="p-input-icon-left">
+          <i class="pi pi-search" />
+          <InputText
+            v-model="filters['global'].value"
+            placeholder="Search..."
+          />
+        </span>
+      </div>
+    </template>
+
+    <Column
+      field="id"
+      header="ID"
+      :sortable="true"
+      style="min-width: 12rem"
+    ></Column>
+    <Column
+      field="fullName"
+      header="Name"
+      :sortable="true"
+      style="min-width: 16rem"
+    ></Column>
+    <Column
+      field="location"
+      header="Location"
+      :sortable="true"
+      style="min-width: 8rem"
+    >
+    </Column>
+    <Column :exportable="false" style="min-width: 8rem">
+      <template #body="slotProps">
+        <SupplierFormDialog
+          :supplier="slotProps.data"
+          :button="{
+            class: 'p-button-rounded p-button-success mr-2',
+            icon: 'pi pi-pencil',
+            label: 'Edit',
+          }"
+        />
+      </template>
+    </Column>
+  </DataTable>
 </template>
 
 <script setup lang="ts">
-import router from '@/router';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useSuppliersStore } from '@/store/suppliersStore';
-import SuppliersTable from '@/components/suppliers/SuppliersTable.vue';
+import SupplierFormDialog from '@/components/suppliers/SupplierFormDialog.vue';
+import { FilterMatchMode } from 'primevue/api';
 
 const store = useSuppliersStore();
 
@@ -18,7 +84,34 @@ await store.fetchAll();
 
 const suppliers = computed(() => store.getAll);
 
-const newSupplier = () => {
-  router.push({ name: 'New Supplier' });
-};
+const filters = ref<{
+  global: {
+    value: null;
+    matchMode: string;
+  };
+}>({
+  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
 </script>
+
+<style lang="scss" scoped>
+.table-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media screen and (max-width: 960px) {
+    align-items: start;
+  }
+}
+
+@media screen and (max-width: 960px) {
+  ::v-deep(.p-toolbar) {
+    flex-wrap: wrap;
+
+    .p-button {
+      margin-bottom: 0.25rem;
+    }
+  }
+}
+</style>
