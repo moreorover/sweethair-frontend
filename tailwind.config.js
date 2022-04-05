46472d85cb7f2b6d7e02fdccace35c0ae711a646
moreorover <mselvenis@gmail.com>
2022-04-05 21:49:44 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Inter var"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
