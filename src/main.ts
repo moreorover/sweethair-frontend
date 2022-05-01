197c8b457ad718014ab187d5a85f2bf988927b06
moreorover <mselvenis@gmail.com>
2022-05-01 22:21:51 +0100

    init

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,7 @@
-import { createPinia } from 'pinia';
-import { createApp } from 'vue';
-import App from './App.vue';
-import './index.css';
-import router from './router';
+import App from './App.svelte'
 
-import DashboardLayout from './components/DashboardLayout.vue';
-import EmptyLayout from './components/EmptyLayout.vue';
+const app = new App({
+  target: document.getElementById('app')
+})
 
-import 'primeflex/primeflex.css';
-import 'primeicons/primeicons.css';
-import 'primevue/resources/primevue.min.css';
-import 'primevue/resources/themes/saga-purple/theme.css';
-import PrimeVue from './utils/primevue';
-
-const app = createApp(App);
-
-app.component('default-layout', DashboardLayout);
-app.component('empty-layout', EmptyLayout);
-
-PrimeVue.register(app);
-app.use(router);
-app.use(createPinia());
-app.mount('#app');
+export default app
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3001,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-});
+  plugins: [svelte()]
+})
