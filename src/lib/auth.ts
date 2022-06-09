f8076fe3a22f281726a09c81081496e538a16132
moreorover <mselvenis@gmail.com>
2022-06-09 12:05:56 +0100

    sign in

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,6 @@
 import { Providers, SvelteKitAuth } from 'sk-auth';
-import type { OAuth2ProviderConfig } from 'sk-auth/dist/providers/oauth2';
+import type { OAuth2Provider, OAuth2ProviderConfig } from 'sk-auth/dist/providers/oauth2';
+import type { OAuth2Tokens } from 'sk-auth/dist/providers/oauth2.base';
 
 // this is the domain we set up in our Cognito Pool
 const DOMAIN = 'sweethair.auth.eu-west-2.amazoncognito.com';
@@ -13,12 +14,21 @@ const config: OAuth2ProviderConfig<any, any> = {
 	clientSecret: import.meta.env.VITE_COGNITO_CLIENT_SECRET,
 	scope: ['openid', 'email'],
 	id: 'cognito', // IMPORTANT: this is the id that we'll use to identify our provider
-	contentType: 'application/x-www-form-urlencoded'
+	contentType: 'application/x-www-form-urlencoded',
+	profile(profile) {
+		return { ...profile, provider: 'cognito' };
+	}
 };
 
-const oauthProvider = new Providers.OAuth2Provider(config);
+const oauthProvider: OAuth2Provider<
+	any,
+	OAuth2Tokens,
+	OAuth2ProviderConfig<any, any>
+> = new Providers.OAuth2Provider(config);
 
 // exposing our auth object
-export const appAuth = new SvelteKitAuth({
+export const appAuth: SvelteKitAuth = new SvelteKitAuth({
 	providers: [oauthProvider]
 });
+
+export const jwt: string = appAuth.getJwtSecret();
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,12 @@
 <script>
 	import { session } from '$app/stores';
 	import { signOut as authSignOut } from 'sk-auth/client';
-
 	// getting the user from the session store
 	$: user = $session.user;
+	$: ses = $session;
 
 	function signIn() {
-		location.assign('/api/auth/callback/cognito?redirect=/');
+		location.assign('/api/auth/signin/cognito?redirect=/?redirect=/');
 	}
 
 	function signOut() {
@@ -20,8 +20,10 @@
 
 {#if !user}
 	<button on:click={signIn}>Log In with Cognito</button>
+	<a href="/api/auth/signin/cognito?redirect=/">Cognito</a>
 {:else}
 	<h2>Welcome {user.email}!</h2>
 	<p>Your username is {user.username} and your email has been verified: {user.email_verified}</p>
 	<button on:click={signOut}>Log Out</button>
+	<p>ses: {JSON.stringify(ses)}</p>
 {/if}
